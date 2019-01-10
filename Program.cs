0d3d79492bf48fab7c7cd940dfab498bf40e0c8c
Адылканов Муратжан Талгатович <@corp.itstep>
2019-01-10 21:42:03 +0600

    Сделал 4 практическую работу

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //тип имя = new Конструктор(параметры);
+            Money money = new Money();
+
+            money.SetCountMoney(10);
+            money.SetParMoney(200);
+            Console.WriteLine("Количество купюр: " + money.GetCountMoney());
+            Console.WriteLine("Номинал купюр: " + money.GetParMoney() + "тг.");
+            Console.WriteLine("Денег у вас: " + money.valueMoney() + "тг.");
+            money.CostProduct(2500);
+            money.CountProduct(500);
+
+            Console.Read();
+        }
+    }
+}
